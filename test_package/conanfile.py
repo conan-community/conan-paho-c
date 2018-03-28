9f1c79027027e92c8d571435b16362c5c0cd0396
danimtb <danimanzaneque@gmail.com>
2018-03-28 12:41:29 +0200

    only run test if not cross-building

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -16,11 +16,12 @@ class TestPackageConan(ConanFile):
         self.copy("*paho*.dylib*", dst="bin", src="lib")
 
     def test(self):
-        with tools.environment_append(RunEnvironment(self).vars):
-            bin_path = os.path.join("bin", "test_package")
-            if self.settings.os == "Windows":
-                self.run(bin_path)
-            elif self.settings.os == "Macos":
-                self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ.get('DYLD_LIBRARY_PATH', ''), bin_path))
-            else:
-                self.run("LD_LIBRARY_PATH=%s %s" % (os.environ.get('LD_LIBRARY_PATH', ''), bin_path))
+        if not tools.cross_building(self.settings):
+            with tools.environment_append(RunEnvironment(self).vars):
+                bin_path = os.path.join("bin", "test_package")
+                if self.settings.os == "Windows":
+                    self.run(bin_path)
+                elif self.settings.os == "Macos":
+                    self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ.get('DYLD_LIBRARY_PATH', ''), bin_path))
+                else:
+                    self.run("LD_LIBRARY_PATH=%s %s" % (os.environ.get('LD_LIBRARY_PATH', ''), bin_path))
