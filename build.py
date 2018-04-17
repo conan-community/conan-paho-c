8a37ce76484c2280d2cb98a164696af97697c010
danimtb <danimanzaneque@gmail.com>
2018-04-17 17:10:42 +0200

    Updated build script

--- build.py
+++ build.py
@@ -1,5 +1,5 @@
 from conan.packager import ConanMultiPackager
-import os, re, platform
+import os, re
 
 
 def get_value_from_recipe(search_string):
@@ -14,22 +14,32 @@ def get_name_from_recipe():
 def get_version_from_recipe():
     return get_value_from_recipe(r'''version\s*=\s*["'](\S*)["']''').groups()[0]
 
+
 if __name__ == "__main__":
+    header_only = False
     name = get_name_from_recipe()
-    username = "conan"
-    channel = "stable"
     version = get_version_from_recipe()
-    login_username = "conanbot"
     reference = "{0}/{1}".format(name, version)
+    username = "conan"
+    login_username = "conanbot"
     upload_remote = "https://api.bintray.com/conan/conan-community/{0}".format(username)
 
     builder = ConanMultiPackager(
+        stable_branch_pattern="stable/*",
+        upload_only_when_stable=True,
         username=username,
-        channel=channel,
         login_username=login_username,
         reference=reference,
         upload=upload_remote,
         remotes=upload_remote)
+
+    if header_only:
+        filtered_builds = []
+        for settings, options, env_vars, build_requires, reference in builder.items:
+            if settings["compiler"] == "gcc":
+                filtered_builds.append([settings, options, env_vars, build_requires])
+                break
+        builder.builds = filtered_builds
 
     builder.add_common_builds(pure_c=True,
                               shared_option_name="paho-c:shared",
