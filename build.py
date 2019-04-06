8dc117278fa7991853deda4824524f56bc88e5bf
Uilian Ries <uilianries@gmail.com>
2019-04-06 19:54:52 -0300

    Fix CI jobs
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -4,5 +4,5 @@ from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds(pure_c=True)
+    builder.add_common_builds(pure_c=True, dll_with_static_runtime=True)
     builder.run()
