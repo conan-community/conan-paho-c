24ba4c6cb001e983fd285aefe8673d7d409dd134
Uilian Ries <uilianries@gmail.com>
2019-04-10 15:56:07 -0300

    Remove connection test
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- test_package/test_package_client.c
+++ test_package/test_package_client.c
@@ -21,38 +21,6 @@
 
 #define ADDRESS     "tcp://iot.eclipse.org:1883"
 #define CLIENTID    "ExampleClientSub"
-#define TOPIC       "MQTT Examples"
-#define PAYLOAD     "Hello World!"
-#define QOS         1
-#define TIMEOUT     10000L
-
-volatile MQTTClient_deliveryToken deliveredtoken;
-
-void delivered(void *context, MQTTClient_deliveryToken dt)
-{
-    printf("Message with token value %d delivery confirmed\n", dt);
-    deliveredtoken = dt;
-}
-
-int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *message)
-{
-    int i;
-    char* payloadptr;
-
-    printf("Message arrived\n");
-    printf("     topic: %s\n", topicName);
-    printf("   message: ");
-
-    payloadptr = message->payload;
-    for(i=0; i<message->payloadlen; i++)
-    {
-        putchar(*payloadptr++);
-    }
-    putchar('\n');
-    MQTTClient_freeMessage(&message);
-    MQTTClient_free(topicName);
-    return 1;
-}
 
 void connlost(void *context, char *cause)
 {
@@ -72,7 +40,7 @@ int main(int argc, char* argv[])
     conn_opts.keepAliveInterval = 20;
     conn_opts.cleansession = 1;
 
-    MQTTClient_setCallbacks(client, NULL, connlost, msgarrvd, delivered);
+    MQTTClient_setCallbacks(client, NULL, connlost, NULL, NULL);
 
     MQTTClient_destroy(&client);
     return rc;
